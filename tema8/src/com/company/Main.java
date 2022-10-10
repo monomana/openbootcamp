125e02bf597acd6e5bcd91d3ba742f5d492fe80c
mjat <mjat.util@gmail.com>
2022-10-09 21:08:28 -0300

    ejercicio tema 8

---
+++ tema8/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Persona persona = new Persona();
+        persona.setEdad(28);
+        persona.setNombre("Juan Perez");
+        persona.setTelefono("385555555");
+
+        System.out.println("Nombre: "+persona.getNombre()+
+                ", Edad: "+persona.getEdad()+", telefono: "+persona.getTelefono());
+    }
+}
